8a283024c64ce7660ecac03ed3b5c32bace9b7ab
AthallahDzaki <AthallahDzaki@gmail.com>
2023-01-19 20:43:27 +0700

    Test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express()
 const port = process.env.port || process.env.PORT || 3000
 
-app.get('/0.3.7/internet', (req, res) => {
+app.get('/', (req, res) => {
   axios.get("https://api.open.mp/servers").then(response => {
     let data = "";
     for(let i = 0; i < response.data.length; i++) { data += response.data[i].ip + '\n'};
